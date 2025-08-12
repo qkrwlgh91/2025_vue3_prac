4ce661c1f2cec10d0d8215d3c132d07649f90549
qkrwlgh91 <qkrwlgh91@gmail.com>
2025-08-13 01:52:13 +0900

    v-bind

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: "remind vue3",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+});
+
+app.mount("#user-goal");
+// app.mount("#events");
+// app.mount("#events");
