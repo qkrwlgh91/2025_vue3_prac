666a18b4c3babcd2b754004ec89eeb34c090e0a2
qkrwlgh91 <qkrwlgh91@gmail.com>
2025-08-13 02:01:58 +0900

    v-on: how to use javascript event

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = Vue.createApp({
     return {
       courseGoal: "remind vue3",
       vueLink: "https://vuejs.org/",
+      counter: 0,
     };
+  },
+  methods: {
+    add() {
+        this.counter = this.counter + 10
+    },
+    reduce() {
+        this.counter = this.counter - 5
+    },
   },
 });
 
-app.mount("#user-goal");
-// app.mount("#events");
+// app.mount("#user-goal");
+app.mount("#events");
 // app.mount("#events");
