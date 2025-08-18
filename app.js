89e82962e01fe8bd13ab8496abdd67fc44edafa7
qkrwlgh91 <qkrwlgh91@gmail.com>
2025-08-18 15:08:02 +0900

    multiple app

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ const app = Vue.createApp({
 
 app.mount("#app");
 
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: "pizza",
+    };
+  },
+});
+
+app2.mount("#app2");
+
 const data = {
   message: "hello!",
   longMessage: "hello world!!!!!!!",
